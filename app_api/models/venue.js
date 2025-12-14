a3e11e19feb6986cdb850be0d535e2aea9458348
omer-ari <omerari2020@gmail.com>
2025-12-14 16:48:15 +0300

    Mekan ve yorum yönetimi için API rotaları ve denetleyicileri uygulandı

--- app_api/models/venue.js
+++ app_api/models/venue.js
@@ -1,21 +1,23 @@
-var mongoose = require('mongoose');
+var mongoose=require("mongoose");
 var hour=new mongoose.Schema({
     day:{type:String,required:true},
-    open: String,
-    close: String,
+    open:{type:String,required:true},
+    close:{type:String,required:true},
     isClosed:{type:Boolean,default:false}
 });
 var comment=new mongoose.Schema({
     author:{type:String,required:true},
-    rating:{type:Number,min:0,max:5,required:true},
+    rating:{type:Number,required:true,min:0,max:5},
     text:{type:String,required:true},
     date:{type:Date,default:Date.now}
 });
-var venue = new mongoose.Schema({
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    rating: { type: Number, default:0, min: 0, max: 5 },
-    foodanddrink: [String],
-    coordinates: { type: [Number], index: '2dsphere' },
+var venue=new mongoose.Schema({
+    name:{type:String,required:true},
+    address:{type:String,required:true},
+   rating:{type:Number,default:0,min:0,max:5},
+   foodanddrink:[String],
+   coordinates:{type:[Number],index:"2dsphere"},
+   hours:[hour],
+   comments:[comment]
 });
-mongoose.model('Venue', venue, 'venues');
+mongoose.model("venue",venue,"venues");
